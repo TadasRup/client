29838099601987b5a8522000fd888a914dafe9b9
Kevinas <129725556+TadasRup@users.noreply.github.com>
2023-08-07 21:34:30 +0300

    page design

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { NoPage } from './pages/NoPage';
+import { BasicLayout } from './layout/BasicLayout';
+import { Register } from './pages/Register';
 
 function App() {
   return (
-   <header>
-   APP
-   </header>
+    <BrowserRouter>
+      <Routes>
+        <Route Component={BasicLayout}>
+          <Route index path='/' element={<Home />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='*' element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
